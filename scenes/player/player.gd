6986c91a9e07167d835fce208667b14aee8bee5e
LiaKatido <aguuux@gmail.com>
2023-02-25 14:23:29 -0300

    added dust particles and crouch

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -14,6 +14,7 @@ const DashTextureLeft = preload("res://assets/particles/dash_inverse.png")
 @onready var dash_audio = $DashAudio
 @onready var dash_particles = $DashParticles
 @onready var attack_marker = $AttackMarker
+@onready var dust_particles = $DustParticles
 
 const SPEED = 85.0
 const JUMP_VELOCITY = -190.0
@@ -24,6 +25,7 @@ const RIGHT = 1
 
 var orientation : Vector2 = Vector2.RIGHT
 var disabled : bool = false
+var was_on_floor : bool = true
 
 # attack data
 const ATTACK_COOLDOWN : float = 0.4
@@ -45,10 +47,20 @@ var dash_ticker : float
 const SPIT_COOLDOWN = 2.0
 var spit_ticker : float
 var can_spit : bool = true
+var spitting : bool = false
+
+# crouch data
+var crouching : bool = false
+var original_sprite_pos : Vector2
+var crouch_sprite_pos : Vector2
 
 
 func _ready():
 	attack_marker_x = attack_marker.position.x
+	
+	# setup crouch data
+	original_sprite_pos = sprite.position
+	crouch_sprite_pos = original_sprite_pos + Vector2(0, 1)
 
 func _unhandled_input(event):
 	if event.is_action_pressed("attack") and can_attack and not dashing:
@@ -59,19 +71,33 @@ func _unhandled_input(event):
 		attack.global_position = attack_marker.global_position
 		attack.do_attack(orientation)
 		animations.play("attack")
+	
 	if event.is_action_pressed("dash") and can_dash:
 		dash_audio.play()
 		can_dash = false
 		animations.play("dash")
 		dashing = true
 		dash_particles.emitting = true
+	
 	if event.is_action_pressed("spit") and can_spit and not dashing:
 		var spit = Spit.instantiate()
 		spit.set_orientation(orientation)
-		can_spit = false
 		spit.global_position = self.global_position + Vector2(orientation.x*5, -10)
 		get_parent().add_child(spit)
+		can_spit = false
+		# spit animation
+		animations.play("shoot")
+		spitting = true
+		
 		spit_audio.play()
+	
+	if event.is_action_pressed("down") and not dashing and not spitting:
+		animations.play("down")
+		crouching = true
+		sprite.position = crouch_sprite_pos
+	
+	if event.is_action_released("down"):
+		reset_after_crouch()
 	
 
 func _process(delta):
@@ -104,6 +130,7 @@ func _process(delta):
 			can_spit = true
 
 func _physics_process(delta):
+	check_floor()
 	if disabled:
 		return
 	
@@ -120,6 +147,8 @@ func _physics_process(delta):
 	# Handle Jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		jump_audio.play()
+		dust_particles.restart()
+		dust_particles.emitting = true
 		velocity.y = JUMP_VELOCITY
 
 	var direction = Input.get_axis("left", "right")
@@ -145,9 +174,37 @@ func _physics_process(delta):
 	
 	# handle animations
 	var attacking = animations.current_animation == "attack" and animations.is_playing()
-	if direction != 0 and is_on_floor() and not attacking:
+	if direction != 0 and is_on_floor() and not attacking and not spitting:
 		animations.play("run")
-	if direction == 0 and is_on_floor() and not attacking:
+	if direction == 0 and is_on_floor() and not attacking and not spitting and not crouching:
+		reset_after_crouch() # sometimes crouch isn't cleaned properly, clean here just in case
 		animations.play("idle")
-	if not is_on_floor() and not attacking:
+	if not is_on_floor() and not attacking and not spitting:
 		animations.play("jump")
+
+# checks if player was touching floor on the previous frame
+# and does what is needed depending the case
+func check_floor():
+	if is_on_floor() and not was_on_floor:
+		dust_particles.restart()
+		dust_particles.emitting = true
+		was_on_floor = true
+	
+	if not is_on_floor() and was_on_floor:
+		was_on_floor = false
+
+func reset_after_crouch():
+	sprite.scale = Vector2(1, 1)
+	sprite.position = original_sprite_pos
+	crouching = false
+
+# handle cleaning after animations finish, when needed
+func _on_animation_player_animation_finished(anim_name):
+	match anim_name:
+		"shoot":
+			spitting = false
+
+
+func _on_animation_player_animation_started(anim_name):
+	if anim_name != "down" and crouching:
+		reset_after_crouch()
