1fc5b351cebffd56ed0bea15a72f5baac19e3179
Kokomaus <KokomausLovesYou@protonmail.com>
2023-02-24 19:21:05 -0500

    implemented UI, reimplemented backgrounds

--- scenes/data.gd
+++ scenes/data.gd
@@ -5,7 +5,8 @@ signal health_changed
 signal max_ammo_changed
 signal ammo_changed
 
-var max_health : int
-var health : int
-var ammo : int
-var max_ammo : int
+var max_health : int = 100
+var health : int = 100
+var ammo : int = 4
+var max_ammo : int = 4
+
---
+++ scenes/menus/uiHandler.gd
@@ -0,0 +1,16 @@
+extends CanvasLayer
+
+@onready var healthBar = get_node("%uiHealthBar")
+@onready var spitBar = get_node("%uiSpit")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	healthBar.max_value = Data.max_health
+	healthBar.value = Data.health
+	spitBar.max_value = Data.max_ammo
+	spitBar.value = Data.ammo
