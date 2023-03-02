8ce17e65628a599018c1d9a59815937ef6224fc4
LiaKatido <aguuux@gmail.com>
2023-03-02 17:51:22 -0300

    save point node added

--- scenes/levels/save_point.gd
+++ scenes/levels/save_point.gd
@@ -4,4 +4,10 @@ func _unhandled_input(event):
 	if event.is_action_pressed("interact") and has_overlapping_areas():
 		var level = get_parent().to_level
 		var gate = get_parent().gate_id
+		
+		# heal player
+		Data.health = Data.max_health
+		
+		# sound
+		
 		Data.save(level, gate)
